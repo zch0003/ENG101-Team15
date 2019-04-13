3ed96598c180a8779753fa207d74cb02f419517e
mtseng15 <tseng.micah@gmail.com>
2019-04-13 09:20:29 -0500

    Commented all GPIOZERO outof magic_fun for testing

--- magic_fun.py
+++ magic_fun.py
@@ -1,22 +1,22 @@
 # Functions needed for Team 15's Magic 8 ball
 
 # Because LED's don't require user interaction, we are importing them here
-from gpiozero import LED
+# from gpiozero import LED
 import subprocess
 import time
 import random
 
 # Instantiate the LEDs
-led1 = LED(4) # Yellow
-led2 = LED(17) # Yellow
-led3 = LED(27) # Yellow
-led4 = LED(22) # Yellow
-led5 = LED(5) # Red
-led6 = LED(6) # Red
-led7 = LED(13) # Green
-led8 = LED(19) # Green
-led9 = LED(26) # Blue
-leds = [led1, led2, led3, led4, led5, led6, led7, led8, led9]
+# led1 = LED(4) # Yellow
+# led2 = LED(17) # Yellow
+# led3 = LED(27) # Yellow
+# led4 = LED(22) # Yellow
+# led5 = LED(5) # Red
+# led6 = LED(6) # Red
+# led7 = LED(13) # Green
+# led8 = LED(19) # Green
+# led9 = LED(26) # Blue
+# leds = [led1, led2, led3, led4, led5, led6, led7, led8, led9]
 
 
 def open_question():
@@ -26,17 +26,17 @@ def open_question():
 def answer():
     print("My answer is yes")
 
-def crazy_LED():
-    for i in leds:
-        i.on()
+# def crazy_LED():
+#     for i in leds:
+#         i.on()
 
-    time.sleep(1)
-    for i in leds:
-        i.off()
-    
+#     time.sleep(1)
+#     for i in leds:
+#         i.off()
 
-def thoughtful_LED():
-    for i in leds:
-        i.on()
-        time.sleep(1)
-        i.off()
+
+# def thoughtful_LED():
+#     for i in leds:
+#         i.on()
+#         time.sleep(1)
+#         i.off()
